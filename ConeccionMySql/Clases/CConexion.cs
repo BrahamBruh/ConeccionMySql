6b78138d65308ed3e095911748b0e88852ef910f
abrah <abrah@TERMINATOR>
2024-06-01 22:26:56 -0600

    Add project files.

---
+++ ConeccionMySql/Clases/CConexion.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using Renci.SshNet.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConeccionMySql.Clases
+{
+    internal class CConexion
+    {
+        MySqlConnection connex = new MySqlConnection();
+        static string servidor = "localhost";
+        static string db = "bdmysql";
+        static string usuario = "root";
+        static string password = " ";
+        static string port = "3306";
+
+
+        string cadenaConexion = "server =" + servidor + ";" + "port=" + port + ";" + "user id=" + usuario + ";" + "password=" + password + ";" + "database=" + db + ";";
+        public MySqlConnection mysqlconnexion()
+        {
+            try {
+                connex.ConnectionString = cadenaConexion;
+                connex.Open();
+                MessageBox.Show("Se conectó a la Data base...");
+
+            
+            }
+            catch (MySqlException e){
+                MessageBox.Show("Imposible conectar a la Base de datos... Error:"+e.ToString());
+            }
+            return connex;
+
+        }
+
+    }
+}
